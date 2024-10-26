f0fcdfab6931fa75d22c5245c34eb24815f37988
kagura7799 <mkotelnikov97@gmail.com>
2024-10-26 14:27:49 +0300

    changed logic

--- include/Sounds.hpp
+++ include/Sounds.hpp
@@ -2,15 +2,20 @@
 #define SOUNDS_HPP
 
 #include <SFML/Audio.hpp>
+#include <map>
+#include <string>
 
-class Sounds
-{
-private:
-    sf::Music music;
-    std::string pathToBackgroundMusic = "/home/kagura/cpp/platformer-game/music/battle.ogg";
-
+class Sounds {
 public:
-    void playBackgroundMusic();
+    void loadSound(const std::string& name, const std::string& filePath, bool loop = false);
+    void playSound(const std::string& name);
+    void stopSound(const std::string& name);
+    void playSoundIfNotPlaying(const std::string& soundName);
+    bool isSoundPlaying(const std::string& name);
+
+private:
+    std::map<std::string, sf::Sound> sounds; 
+    std::map<std::string, sf::SoundBuffer> soundBuffers;
 };
 
 #endif
--- src/Sounds.cpp
+++ src/Sounds.cpp
@@ -1,13 +1,44 @@
 #include "Sounds.hpp"
 #include <iostream>
 
-void Sounds::playBackgroundMusic()
-{
-    if (!music.openFromFile(pathToBackgroundMusic)) {
-        std::cerr << "Failed loading music: " << std::endl;
+void Sounds::loadSound(const std::string& name, const std::string& filePath, bool loop) {
+    sf::SoundBuffer buffer;
+    if (!buffer.loadFromFile(filePath)) {
+        std::cerr << "Failed to load sound: " << filePath << std::endl;
         return;
     }
-    
-    music.setLoop(true);
-    music.play();
+
+    soundBuffers[name] = buffer; 
+    sf::Sound sound;
+    sound.setBuffer(soundBuffers[name]);
+    sound.setLoop(loop); 
+    sounds[name] = sound;
+}
+
+void Sounds::playSound(const std::string& name) {
+    auto it = sounds.find(name);
+    if (it != sounds.end()) {
+        it->second.play();
+    }
+}
+
+void Sounds::stopSound(const std::string& name) {
+    auto it = sounds.find(name);
+    if (it != sounds.end()) {
+        it->second.stop();
+    }
+}
+
+void Sounds::playSoundIfNotPlaying(const std::string& soundName) {
+    if (!isSoundPlaying(soundName)) {
+        playSound(soundName);
+    }
+}
+
+bool Sounds::isSoundPlaying(const std::string& name) {
+    auto it = sounds.find(name);
+    if (it != sounds.end()) {
+        return it->second.getStatus() == sf::Sound::Playing;
+    }
+    return false;
 }
